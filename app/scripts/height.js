395d205fa185438a16059092a8f14500da3f891a
dawid.kossowski <dawidkossowski@gmail.com>
2017-04-15 20:33:17 +0200

    second page

--- app/scripts/height.js
+++ app/scripts/height.js
@@ -2,3 +2,35 @@ var footer = $('.footer');
 
 var h = $(window).height() - footer.height();
 footer.css('top', h);
+
+
+// AUTO SCROLL AFTER CHANGE HEIGHT
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+
+var sectionHeight = $(".main").outerHeight();
+var actualSection = 0;
+var height = 0, isAnimate = false;
+
+if(!isMobile) {
+    $(window).scroll(function() {
+        height = $(window).scrollTop();
+
+        if((height  > actualSection * sectionHeight + 20) && !isAnimate) {
+            actualSection += 1;
+            isAnimate = true;
+            $('html, body').animate({ scrollTop: sectionHeight * actualSection + 'px' }, 900, 'swing', function() {
+                isAnimate = false;
+            });
+        }
+
+        if((height  < actualSection * sectionHeight - 20) && !isAnimate) {
+            actualSection -= 1;
+            isAnimate = true;
+            $('html, body').animate({ scrollTop: sectionHeight * actualSection + 'px' }, 900, 'swing', function() {
+                isAnimate = false;
+            });
+        }
+    });
+}
+
+
