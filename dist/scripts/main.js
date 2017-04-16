56159ddf611dafd98fb7ac8feb84481b58e82570
dawid.kossowski <dawidkossowski@gmail.com>
2017-04-16 23:47:02 +0200

    zmiana mechanizmu scrollowania sekcji

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,15 +1,16 @@
-var footer = $('.footer');
+/*var footer = $('.footer');
 
-var h = $(window).height() - footer.height();
-footer.css('top', h);
+var footerPosition = $(document).height() - 2 * footer.height() -10;
+footer.css('top', footerPosition);
 
 $(window).resize(function() {
-    h = $(window).height() - footer.height();
-    footer.css('top', h);
+    footerPosition = $(document).height() - footer.height();
+    footer.css('top', footerPosition);
 })
-
+*/
 
 // AUTO SCROLL AFTER CHANGE HEIGHT
+/*
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
 
 var sectionHeight = $(".main").outerHeight();
@@ -37,9 +38,47 @@ if(!isMobile) {
         }
     });
 }
+*/
+
+/*
+var sections = $(".main");
+var actualSection = 0;
+var height = 0, isAnimate = false;
+var actualHeight = 0;
+
+$(window).scroll(function() {
+    height = $(window).scrollTop();
+    var sectionHeight = $(sections[actualSection]).outerHeight();
+
+    if((height  > actualHeight + sectionHeight - $(window).height() + 20) && !isAnimate) {
+        actualHeight +=  sectionHeight;
+        actualSection += 1;
+        isAnimate = true;
+        $('html, body').animate({ scrollTop: actualSection * sectionHeight + 'px' }, 900, 'swing', function() {
+            isAnimate = false;
+        });
+    }
+
+    if((height  < actualHeight - 20) && !isAnimate) {
+        actualSection -= 1;
+        isAnimate = true;
+        $('html, body').animate({ scrollTop: actualHeight - $(sections[actualSection]).outerHeight()  + 'px' }, 900, 'swing', function() {
+            isAnimate = false;
+            actualHeight = height;
+        });
+    }
+    
+});
+*/
 
 
-
+$('#fullpage').fullpage({
+    'sectionsColor': ['#fff', '#0079bf'],
+    'verticalCentered': true,
+    'navigation': true,
+    'navigationPosition': 'right',
+    'loopBottom': true
+});
 
 var hamburger = $("#menu-hamburger");
 var menu = $("#menu");
